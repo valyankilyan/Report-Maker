5f1201fb26a53926d48fd68afcc7d9cb9f2837d7
Valentin <valentin.kil.2002@gmail.com>
2021-10-08 18:09:07 +0300

    delete debug prints

--- docxreplacer.py
+++ docxreplacer.py
@@ -1,16 +1,15 @@
 from docx import Document
-from config import replace_dictionary 
+from config import replace_dictionary
 
 document = Document("title_template.docx")
 
 for p in document.paragraphs:
     if p.text != '':
-        for c in replace_dictionary:            
-            if p.text.find(c) >= 0:            
-                for i in range(len(p.runs)):                    
+        for c in replace_dictionary:
+            if p.text.find(c) >= 0:
+                for i in range(len(p.runs)):
                     if c in p.runs[i].text:
-                        p.runs[i].text = p.runs[i].text.replace(c, replace_dictionary[c])                                           
-            
+                        p.runs[i].text = p.runs[i].text.replace(
+                            c, replace_dictionary[c])
+
 document.save("out.docx")
-            
-    
