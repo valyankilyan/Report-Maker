c0cfb68a6eecdd4eaaec4981e3a71b73ab60c9db
Valentin <valentin.kil.2002@gmail.com>
2021-10-08 17:53:12 +0300

    fix parsing bug

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ with open(title_keys_path) as f:
     for line in f:
         if line.startswith('#') or not line.strip():
             continue
-        key, value = line.split('=', 1)
-        replace_dictionary[key] = value
+        key, value = line.replace('\n', '').split('=', 1)
+        replace_dictionary[key] = str(value)
 
