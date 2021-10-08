8d8bdafd362e599d03f8364e085a4d7ce91d0b2b
Valentin <valentin.kil.2002@gmail.com>
2021-10-08 18:08:48 +0300

    delete debug prints

--- docxreplacer.py
+++ docxreplacer.py
@@ -7,11 +7,9 @@ for p in document.paragraphs:
     if p.text != '':
         for c in replace_dictionary:            
             if p.text.find(c) >= 0:            
-                for i in range(len(p.runs)):                
-                    print(p.runs[i].text)
+                for i in range(len(p.runs)):                    
                     if c in p.runs[i].text:
-                        p.runs[i].text = p.runs[i].text.replace(c, replace_dictionary[c])
-                        print(f'{c} was succesfully found')                        
+                        p.runs[i].text = p.runs[i].text.replace(c, replace_dictionary[c])                                           
             
 document.save("out.docx")
             
