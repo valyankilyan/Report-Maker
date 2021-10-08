06d074b08e47157bb3a6f505d5e16c5f9b867eb3
Valentin <valentin.kil.2002@gmail.com>
2021-10-08 18:01:12 +0300

    docxreplacer added

---
+++ docxreplacer.py
@@ -0,0 +1,18 @@
+from docx import Document
+from config import replace_dictionary 
+
+document = Document("title_template.docx")
+
+for p in document.paragraphs:
+    if p.text != '':
+        for c in replace_dictionary:            
+            if p.text.find(c) >= 0:            
+                for i in range(len(p.runs)):                
+                    print(p.runs[i].text)
+                    if c in p.runs[i].text:
+                        p.runs[i].text = p.runs[i].text.replace(c, replace_dictionary[c])
+                        print(f'{c} was succesfully found')                        
+            
+document.save("out.docx")
+            
+    
